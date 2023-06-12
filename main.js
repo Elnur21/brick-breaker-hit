ce3dbb631b0bf3988b9748cfddd2e03b8d2bb9f1
Elnur21 <elnurme@code.edu.az>
2023-06-13 00:01:55 +0400

    pause function

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ let bar = document.querySelector("#bar");
 let start = document.querySelector("#start");
 let pause = document.querySelector("#pause");
 let barLeft = 200;
-let ballDirection = 1;
+let ballDirectionX = 1;
+let ballDirectionY = 1;
 let ballLeft = 0;
+let ballTop = 0;
 // let ballLeft = Math.random()*10
 bar.style.left = barLeft + "px";
 ball.style.left = ballLeft + "px";
@@ -20,13 +22,24 @@ document.addEventListener("keydown", (e) => {
 });
 
 start.addEventListener("click", () => {
-  setInterval(()=>{
+  let interval = setInterval(() => {
     if (ballLeft === 470) {
-        ballDirection = -1;
-      } else if (ballLeft === 0) {
-        ballDirection = 1;
-      }
-      ballLeft += ballDirection;
-      ball.style.left = ballLeft + "px";
-  },5)
+      ballDirectionX = -1;
+    } else if (ballLeft === 0) {
+      ballDirectionX = 1;
+    }
+    if (ballTop === 470) {
+      ballDirectionY = -1;
+    } else if (ballTop === 0) {
+      ballDirectionY = 1;
+    }
+    ballLeft += ballDirectionX;
+    ballTop += ballDirectionY;
+    ball.style.left = ballLeft + "px";
+    ball.style.top = ballTop + "px";
+  }, 5);
+  pause.addEventListener("click",()=>{
+    clearInterval(interval)
+})
 });
+
