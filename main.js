de3313522f3c5f6d83c07d5d31ff134f41c43af2
Elnur21 <elnurme@code.edu.az>
2023-06-13 12:10:01 +0400

    add localStorage part

--- main.js
+++ main.js
@@ -4,24 +4,27 @@ let start = document.querySelector("#start");
 let pause = document.querySelector("#pause");
 let restart = document.querySelector("#restart");
 let bricks = document.querySelectorAll("ul li");
-let itemLeft = 0
-let itemTop = 0
-bricks.forEach((item)=>{
-    item.style.left=itemLeft+"px"
-    item.style.top=itemTop+"px"
-    if(itemLeft===450){
-        itemLeft=0
-        itemTop+=20
-    }
-    else{
-        itemLeft+=50;
-    }
-})
+let scores = document.querySelector("#scores");
+let score = document.querySelector("#score");
+let scoresArray = [];
+let scoreCount = 0;
+let itemLeft = 0;
+let itemTop = 0;
+bricks.forEach((item) => {
+  item.style.left = itemLeft + "px";
+  item.style.top = itemTop + "px";
+  if (itemLeft === 450) {
+    itemLeft = 0;
+    itemTop += 20;
+  } else {
+    itemLeft += 50;
+  }
+});
 let barLeft = 200;
 let ballDirectionX = 1;
 let ballDirectionY = 1;
-let ballLeft = Math.floor(Math.random() * 300);
-let ballTop = Math.floor(Math.random() * 300);
+let ballLeft = 235;
+let ballTop = 447;
 bar.style.left = barLeft + "px";
 ball.style.left = ballLeft + "px";
 ball.style.top = ballTop + "px";
@@ -38,22 +41,45 @@ document.addEventListener("keydown", (e) => {
 
 start.addEventListener("click", () => {
   let interval = setInterval(() => {
+    let directionX = Math.floor(Math.random() * 10);
     if (ballLeft === 470) {
       ballDirectionX = -1;
     } else if (ballLeft === 0) {
       ballDirectionX = 1;
     }
-    if (ballTop === 460 && ballLeft >= barLeft-30 && ballLeft <= barLeft + 115) {
+    if (
+      ballTop === 460 &&
+      ballLeft >= barLeft - 15 &&
+      ballLeft <= barLeft + 115
+    ) {
       ballDirectionY = -1;
+      ballDirectionX = directionX < 4 ? 1 : directionX < 7 ? -1 : 0;
     } else if (ballTop > 470) {
+      scoresArray.push(`Uduzdu ${scoreCount} xal ile`);
+      localStorage.setItem("scores", JSON.stringify(scoresArray));
       clearInterval(interval);
+    } else if (ballTop === 0) {
+      ballDirectionY = 1;
     }
     for (let i = 0; i < bricks.length; i++) {
-        if(ballLeft >= (bricks[i].offsetLeft-10) && ballLeft <= (bricks[i].offsetLeft+60) && ballTop === bricks[i].offsetTop){
-            bricks[i].style.display="none";
-            ballDirectionY=1
-            break
-        }
+      if (scoreCount === bricks.length) {
+        scoresArray.push(`Max xal ile uddu`);
+        localStorage.setItem("scores", JSON.stringify(scoresArray));
+        clearInterval(interval);
+        break;
+      }
+      if (
+        ballLeft >= bricks[i].offsetLeft - 15 &&
+        ballLeft <= bricks[i].offsetLeft + 65 &&
+        ballTop === bricks[i].offsetTop
+      ) {
+        bricks[i].style.display = "none";
+        ballDirectionY = 1;
+        ballDirectionX = directionX < 4 ? 1 : directionX < 7 ? -1 : 0;
+        scoreCount += 1;
+        score.innerHTML = scoreCount;
+        break;
+      }
     }
     ballLeft += ballDirectionX;
     ballTop += ballDirectionY;
@@ -65,10 +91,29 @@ start.addEventListener("click", () => {
   });
   restart.addEventListener("click", () => {
     clearInterval(interval);
-    ballLeft = Math.floor(Math.random() * 300);
-    ballTop = Math.floor(Math.random() * 300);
+    ballLeft = 235;
+    ballTop = 447;
+    barLeft = 200;
     bar.style.left = barLeft + "px";
     ball.style.left = ballLeft + "px";
     ball.style.top = ballTop + "px";
+    bricks.forEach((item) => {
+      item.style.display = "block";
+    });
+    reset();
   });
 });
+
+onload(reset());
+
+function reset() {
+  scoreCount = 0;
+  score.innerHTML = scoreCount;
+  scores.innerHTML = "";
+  scoresArray = JSON.parse(localStorage.getItem("scores"));
+  scoresArray.forEach((score) => {
+    scores.innerHTML += `
+      <p>${score}</p>
+      `;
+  });
+}
